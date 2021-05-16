46aaf8b488f7e6ff93faa28672f3947287779a36
techNcody <abiswas392@gmail.com>
2021-05-16 13:53:21 +0530

    Changed database access port

--- public/js/login.js
+++ public/js/login.js
@@ -20,62 +20,62 @@ if (verifyOtpForm) {
 
 const login = async (otp) => {
   try {
-    $.ajax({
-      url: '/api/v2/users/login',
-      dataType: 'json',
-      type: 'Post',
-      async: true,
-      data: { otp },
-      success: function (data) {
-        console.log(data);
-        if (data.data.status === 'success') {
-          window.setTimeout(() => {
-            location.assign('/home.html');
-          }, 3000);
-        }
-      },
-      error: function (xhr, exception, thrownError) {
-        var msg = '';
-        if (xhr.status === 0) {
-          msg = 'Not connect.\n Verify Network.';
-        } else if (xhr.status == 404) {
-          msg = 'Requested page not found. [404]';
-        } else if (xhr.status == 500) {
-          msg = 'Internal Server Error [500].';
-        } else if (exception === 'parsererror') {
-          msg = 'Requested JSON parse failed.';
-        } else if (exception === 'timeout') {
-          msg = 'Time out error.';
-        } else if (exception === 'abort') {
-          msg = 'Ajax request aborted.';
-        } else {
-          msg = 'Error:' + xhr.status + ' ' + xhr.responseText;
-        }
-        alert(msg);
-      }
-    });
-
-    // const result = await axios({
-    //   method: 'POST',
+    // $.ajax({
     //   url: '/api/v2/users/login',
-    //   data: {
-    //     otp
+    //   dataType: 'json',
+    //   type: 'Post',
+    //   async: true,
+    //   data: { otp },
+    //   success: function (data) {
+    //     console.log(data);
+    //     if (data.data.status === 'success') {
+    //       window.setTimeout(() => {
+    //         location.assign('/home.html');
+    //       }, 3000);
+    //     }
+    //   },
+    //   error: function (xhr, exception, thrownError) {
+    //     var msg = '';
+    //     if (xhr.status === 0) {
+    //       msg = 'Not connect.\n Verify Network.';
+    //     } else if (xhr.status == 404) {
+    //       msg = 'Requested page not found. [404]';
+    //     } else if (xhr.status == 500) {
+    //       msg = 'Internal Server Error [500].';
+    //     } else if (exception === 'parsererror') {
+    //       msg = 'Requested JSON parse failed.';
+    //     } else if (exception === 'timeout') {
+    //       msg = 'Time out error.';
+    //     } else if (exception === 'abort') {
+    //       msg = 'Ajax request aborted.';
+    //     } else {
+    //       msg = 'Error:' + xhr.status + ' ' + xhr.responseText;
+    //     }
+    //     alert(msg);
     //   }
     // });
-    // console.log(result.data.data.status);
 
-    // if (result.data.data.status === 'success') {
-    //   // console.log('logged in successfully');
-    //   // showAlert('Logged in successfully', 1500);
-    //   window.setTimeout(() => {
-    //     location.assign('/home.html');
-    //   }, 3000);
-    // } else {
-    //   alert('Invalid OTP!!');
-    //   // window.setTimeout(() => {
-    //   //   location.assign('/');
-    //   // }, 3000);
-    // }
+    const result = await axios({
+      method: 'POST',
+      url: '/api/v2/users/login',
+      data: {
+        otp
+      }
+    });
+    console.log(result.data.data.status);
+
+    if (result.data.data.status === 'success') {
+      // console.log('logged in successfully');
+      // showAlert('Logged in successfully', 1500);
+      window.setTimeout(() => {
+        location.assign('/home.html');
+      }, 3000);
+    } else {
+      alert('Invalid OTP!!');
+      // window.setTimeout(() => {
+      //   location.assign('/');
+      // }, 3000);
+    }
   } catch (err) {
     showAlert('Incorrect email or password!!', 1500);
     console.log(err);
@@ -96,51 +96,51 @@ if (getOtpForm) {
 
 async function getOtpFn(email) {
   try {
-    $.ajax({
-      url: '/api/v2/users/sendOtp',
-      dataType: 'json',
-      type: 'Post',
-      async: true,
-      data: { email },
-      success: function (data) {
-        alert('An OTP has been sent to your registered email id');
-        console.log(data);
-        getOtpForm.classList.add('dl-hide-el');
-        verifyOtpForm.classList.remove('dl-hide-el');
-      },
-      error: function (xhr, exception, thrownError) {
-        var msg = '';
-        if (xhr.status === 0) {
-          msg = 'Not connect.\n Verify Network.';
-          console.log(xhr.responseText);
-        } else if (xhr.status == 404) {
-          msg = 'Requested page not found. [404]';
-        } else if (xhr.status == 500) {
-          msg = 'Internal Server Error [500].';
-        } else if (exception === 'parsererror') {
-          msg = 'Requested JSON parse failed.';
-        } else if (exception === 'timeout') {
-          msg = 'Time out error.';
-        } else if (exception === 'abort') {
-          msg = 'Ajax request aborted.';
-        } else {
-          msg = 'Error:' + xhr.status + ' ' + xhr.responseText;
-        }
-        alert(msg);
-      }
-    });
-
-    // const result = await axios({
-    //   method: 'POST',
+    // $.ajax({
     //   url: '/api/v2/users/sendOtp',
-    //   data: {
-    //     email
+    //   dataType: 'json',
+    //   type: 'Post',
+    //   async: true,
+    //   data: { email },
+    //   success: function (data) {
+    //     alert('An OTP has been sent to your registered email id');
+    //     console.log(data);
+    //     getOtpForm.classList.add('dl-hide-el');
+    //     verifyOtpForm.classList.remove('dl-hide-el');
+    //   },
+    //   error: function (xhr, exception, thrownError) {
+    //     var msg = '';
+    //     if (xhr.status === 0) {
+    //       msg = 'Not connect.\n Verify Network.';
+    //       console.log(xhr.responseText);
+    //     } else if (xhr.status == 404) {
+    //       msg = 'Requested page not found. [404]';
+    //     } else if (xhr.status == 500) {
+    //       msg = 'Internal Server Error [500].';
+    //     } else if (exception === 'parsererror') {
+    //       msg = 'Requested JSON parse failed.';
+    //     } else if (exception === 'timeout') {
+    //       msg = 'Time out error.';
+    //     } else if (exception === 'abort') {
+    //       msg = 'Ajax request aborted.';
+    //     } else {
+    //       msg = 'Error:' + xhr.status + ' ' + xhr.responseText;
+    //     }
+    //     alert(msg);
     //   }
     // });
-    // alert('An OTP has been sent to your registered email id');
-    // console.log(result.data.data);
-    // getOtpForm.classList.add('dl-hide-el');
-    // verifyOtpForm.classList.remove('dl-hide-el');
+
+    const result = await axios({
+      method: 'POST',
+      url: '/api/v2/users/sendOtp',
+      data: {
+        email
+      }
+    });
+    alert('An OTP has been sent to your registered email id');
+    console.log(result.data.data);
+    getOtpForm.classList.add('dl-hide-el');
+    verifyOtpForm.classList.remove('dl-hide-el');
   } catch (err) {
     alert('Invalid Email!!');
     console.log(err);
