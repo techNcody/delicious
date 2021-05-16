f71f1d211cb25629f4a0edc9a1c93ad1c9607588
techNcody <abiswas392@gmail.com>
2021-05-16 13:24:00 +0530

    first commit 2

--- app.js
+++ app.js
@@ -19,29 +19,26 @@ app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
-// CORS handling
+// Add headers
 app.use(function (req, res, next) {
-  /*var err = new Error('Not Found');
-   err.status = 404;
-   next(err);*/
-  /////////////////////////////////////////////////////////////////////////////
-  // // Website you wish to allow to connect
-  // res.setHeader('Access-Control-Allow-Origin', '*');
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3030');
 
-  // // Request methods you wish to allow
-  // res.setHeader(
-  //   'Access-Control-Allow-Methods',
-  //   'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  // );
+  // Request methods you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
 
-  // // Request headers you wish to allow
-  // res.setHeader(
-  //   'Access-Control-Allow-Headers',
-  //   'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization'
-  // );
-  /////////////////////////////////////////////////////////////////////////////
+  // Request headers you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
 
-  //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
 
   // Pass to next layer of middleware
   next();
